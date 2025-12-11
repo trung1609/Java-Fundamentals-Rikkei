5eb393505e884336d0270647eff57cae8dd5defb
trung1609 <vmt162005@gmail.com>
2025-12-11 15:45:24 +0700

    pull code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        StringBuilder sb = new StringBuilder();
+        sb.append("Hello World");
+        System.out.println(sb);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        StringBuffer sb1 = new StringBuffer();
+        sb1.append("Hello World");
+        System.out.println(sb1);
+
+        StringBuffer sb2 = new StringBuffer("Hello");
+        sb2.replace(1, 2, "Java");
+        System.out.println(sb2);
     }
 }
